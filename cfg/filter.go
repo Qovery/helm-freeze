9286e42b4df04fe5fb7f5e9863c7e4f8e6f1f7c0
Jan Pieper <kontakt@jan-pieper.info>
2022-05-11 13:46:22 +0200

    feat: allow partial syncs (#3)

---
+++ cfg/filter.go
@@ -0,0 +1,30 @@
+package cfg;
+
+func FilterCharts(config Config, charts []string) Config {
+	chartsToFilter := make(map[string]bool)
+	reposToFilter := make(map[string]bool)
+
+	for _, name := range charts {
+		chartsToFilter[name] = true
+	}
+
+	var filteredCharts []map[string]string
+	for _, chart := range config.Charts {
+		if chartsToFilter[chart["name"]] {
+			filteredCharts = append(filteredCharts, chart)
+			reposToFilter[chart["repo_name"]] = true
+		}
+	}
+
+	var filteredRepos []map[string]string
+	for _, repo := range config.Repos {
+		if reposToFilter[repo["name"]] {
+			filteredRepos = append(filteredRepos, repo)
+		}
+	}
+
+	config.Charts = filteredCharts
+	config.Repos = filteredRepos
+
+	return config
+}
